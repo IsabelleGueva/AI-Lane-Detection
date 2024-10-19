3a10addea5d968d28ead9ce855d42c64e1df7d72
IsabelleGueva <146399703+IsabelleGueva@users.noreply.github.com>
2024-10-19 12:42:07 -0700

    initial commit
    initial commit: multi-line comment on what to do this week, otherwise blank

---
+++ laneLineAlgorithm.py
@@ -0,0 +1,5 @@
+"""Hi everyone! Use examples, references, and any resources you need to code a 
+function that covers the topic you chose on the 10/19 Meeting Minutes. Use lots of 
+comments to explain your code. If you need any help, make note of it and we can brainstorm next meeting. 
+Don't merge your branches until after our code review.
+Good luck everyone!"""
