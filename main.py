aacad49e767cac3b5c2a1b0df89e59e448ed6a71
Hasti Abbasi Kenarsari <hastiabbasikenarsari@gmail.com>
2024-10-26 14:10:05 -0700

    Added the skeleton file to allow all components to be integrated together

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+# import packages
+
+# image preprocessing
+
+# edge detection 
+
+# region of interest selection 
+
+# detect lines with Hough Transform 
+
+# draw the lines & combine with original image 
+
+# process image & final return 
