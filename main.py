479615d43911491d2c91acfd3c6c4e205302ffa9
IsabelleGueva <146399703+IsabelleGueva@users.noreply.github.com>
2024-11-11 21:10:31 -0800

    add Hough Transform to Isabelle branch

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ import cv2
 # region of interest selection 
 
 # detect lines with Hough Transform 
-
+def hough_transform(image): 
+    rho = 1              #looks at every pixels surrounding it to find lines
+    theta = np.pi/180    #finds the pixels within a 1-pixel precision and finds angles within 1-degree precision
+    threshold = 10       #Only lines that are greater than threshold will be returned; must be within 1-degree so it can still be considered “straight” line
+    minLineLength = 10   #Line segments shorter than that are rejected; in this case, longer than 20 pixels
+    maxLineGap = 350     #Maximum allowed gap between points on the same line to link them; necessary for broken lane lines
+    return cv2.HoughLinesP(image, rho = rho, theta = theta, threshold = threshold, minLineLength = minLineLength, maxLineGap = maxLineGap)
 # draw the lines & combine with original image 
 
 # process image & final return 
